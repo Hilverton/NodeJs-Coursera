7e863269aa5645242f8574e3e77f244c7c3d7dd0
Emusc <44450158+Hilverton@users.noreply.github.com>
2020-05-18 13:56:45 -0300

    Express Serve Static Files

--- Semana 1/Exercícios/Express/index.js
+++ Semana 1/Exercícios/Express/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const morgan = require('morgan');
 const http = require('http');
 
 const host = 'localhost';
 const port = 3000;
 
 const app = express();
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
-    console.log(req.headers);
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('<html><body><h1>This is a Express Server</h1></body></html>')
 });
@@ -16,4 +19,6 @@ const server = http.createServer(app);
 
 server.listen(port, host, () => {
     console.log(`Server running at http://${host}:${port}`);
-})
+});
+
+//ao fazer a requisição, vai ser direcionado para a index.html dentro da pasta public. Para ir para a segunda configuração, o html no terceira app.use, tem que ser uma rota em que o sercidor não encontra o arquivo html. Não é a maneira ideal de se fazer isso. 

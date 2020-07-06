22776bb90a4ba4dd39f7360fe2deefa5b176224a
Emusc <44450158+Hilverton@users.noreply.github.com>
2020-07-06 19:40:47 -0300

    Mongoose Part 3

--- Semana 2/Exercícios/Node-Mongoose/index.js
+++ Semana 2/Exercícios/Node-Mongoose/index.js
@@ -8,16 +8,32 @@ connect.then(() => {
     console.log('Connected correctly to server');
 
     Dishes.create({
-        name: 'Uthappizza',
+        name: 'Pizza',
         description: 'test'
     })
     .then(dish => {
         console.log(dish);
 
-        return Dishes.find({});
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'}
+        },{ 
+            new: true 
+        })
+        .exec();
     })
-    .then(dishes => {
-        console.log(dishes);
+    .then(dish => {
+        console.log('dish:',dish);
+        
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
+
+        return dish.save();
+    })
+    .then(dish => {
+        console.log(dish);
 
         return Dishes.deleteOne({});
     })
